ce0de9adc066e42ead5db9e8737b307f62bd7b79
Zeb-L <60537220+Zeb-L@users.noreply.github.com>
2023-07-20 16:00:07 +0800

    Update ubuntu.sh

--- Scripts/Installer/Ubuntu/ubuntu.sh
+++ Scripts/Installer/Ubuntu/ubuntu.sh
@@ -24,7 +24,7 @@ if [ "$first" != 1 ];then
 		*)
 			echo "unknown architecture"; exit 1 ;;
 		esac
-		wget "https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Rootfs/Ubuntu/${archurl}/ubuntu-rootfs-${archurl}.tar.xz" -O $tarball
+		wget "https://raw.githubusercontents.com/Zeb-L/AnLinux-Resources/master/Rootfs/Ubuntu/${archurl}/ubuntu-rootfs-${archurl}.tar.xz" -O $tarball
 	fi
 	cur=`pwd`
 	mkdir -p "$folder"
